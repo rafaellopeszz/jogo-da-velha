228502959abb1982d55dcb75a56bd790f3a37019
Rafael Lopes <rafael.rlopes96@gmail.com>
2021-07-02 19:42:09 -0300

    1° commit projeto jogo da velha

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto desenvolvido com o intuito de colocar em pratica conhecimentos obtidos com a linguaguem de programação JavaScript
---
+++ scripts/interface.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+  let squares = document.querySelectorAll('.square')
+
+  squares.forEach(square => {
+    square.addEventListener('click', handleClick)
+  })
+})
+
+function handleClick(event) {
+  let square = event.target
+  let position = square.id
+
+  if (handleMove(position) == true) {
+    setTimeout(() => {
+      alert('O Jogo Acabou! O vencedor foi o Jogador: ' + playerTime)
+    }, 10)
+  }
+  updateSquares()
+}
+
+function updateSquares() {
+  let squares = document.querySelectorAll('.square')
+
+  squares.forEach(square => {
+    let position = square.id
+    let symbol = board[position]
+
+    if (symbol != '') {
+      square.innerHTML = `<div class='${symbol}'></div>`
+    }
+  })
+}
